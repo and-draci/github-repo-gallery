9326f442a573ced4378bae3c6209d5d0d8e920f7
and_draci <draciannanderson@gmail.com>
2022-12-22 01:46:37 -0500

    Pull repo  information from github to populate the website.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 //Query selects the overview class with profile information.
 const overview = document.querySelector(".overview");
 const username = "and-draci";
+const repoList = document.querySelector(".repo-list");
 
+// Fetches repos with user info.
 const gitUserInfo = async function (){
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
     const data = await userInfo.json();
@@ -12,6 +14,7 @@ userInfoDisplay(data);
 
 gitUserInfo();
 
+// Display user info from Github on webpage
 const userInfoDisplay= function(data){
     const userInfoDiv= document.createElement("div"); 
     userInfoDiv.classList.add("user-info");
@@ -26,5 +29,27 @@ const userInfoDisplay= function(data){
   </div> `;
 
   overview.append(userInfoDiv);
+  
+  gitRepos();
+};
+
+// Gets repo info
+const gitRepos = async function(){
+    const fetchRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+   // console.log(repoData);
+
+
+   displayRepos(repoData);
+};
+
+//Displays repo info on webpage.
+const displayRepos= function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
 
